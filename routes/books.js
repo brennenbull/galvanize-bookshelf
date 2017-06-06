e9db1fead453d7c92a29dbd353761ba91bd11c8a
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-05 19:09:19 -0600

    parts 1-3

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,45 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const humps = require('humps');
 
-// YOUR CODE HERE
+router.get('/books', (req, res, next)=>{
+  knex('books').orderBy('title').then((book)=>{
+    res.send(humps.camelizeKeys(book));
+  });
+});
+
+router.get('/books/:id', (req, res, next)=>{
+  let id = req.params.id;
+  knex('books').where('id', id).then((book)=>{
+    res.send(humps.camelizeKeys(book)[0]);
+  });
+});
+
+router.post('/books', (req, res, next)=>{
+  knex('books').insert(humps.decamelizeKeys(req.body)).returning('*').then((books)=>{
+    res.send(humps.camelizeKeys(books[0]));
+  });
+});
+
+router.patch('/books/:id', (req, res, next)=>{
+  let id = req.params.id;
+  let bookObj = req.body;
+  bookObj.cover_url = bookObj.coverUrl;
+  delete bookObj.coverUrl;
+  knex('books').where('id', id).returning('*').update(bookObj).then((book)=>{
+    res.send(humps.camelizeKeys(book[0]));
+  });
+});
+
+router.delete('/books/:id', (req, res, next)=>{
+  let id = req.params.id;
+  knex('books').where('id', id).returning('*').del().then((book)=>{
+    let bookObj = humps.camelizeKeys(book[0]);
+    delete bookObj.id;
+    res.send(bookObj);
+  });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const express = require('express');
+const humps = require('humps');
+const knex = require('../knex');
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
+function hash(plaintext) {
+  return bcrypt.hashSync(plaintext,8);
+}
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
-
-// YOUR CODE HERE
+router.post('/users', (req, res, next)=>{
+  let hashpass = hash(req.body.password);
+  let userData = humps.decamelizeKeys(req.body);
+  userData.hashed_password = hashpass;
+  delete userData.password;
+  knex('users').insert(userData).returning('*').then((user)=>{
+    let obj = humps.camelizeKeys(user[0]);
+    delete obj.hashedPassword;
+    res.send(obj);
+  });
+});
 
 module.exports = router;
