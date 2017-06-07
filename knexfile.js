6296ee30aabdc30764211512ecce49bf62082e00
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-07 16:23:18 -0600

    makeing evns

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,6 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: 'postgres://rbtsoenkyjeyci:e0611d4c6cb4465d05490d739ea4fc142c8a17a8a4beb1c110b51536835585f5@ec2-107-21-99-176.compute-1.amazonaws.com:5432/d2o2gr3476pmk6'
+    connection: process.env.DATABASE_URL
   }
 };
